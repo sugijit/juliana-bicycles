585301600a02554b6712194dfd7ae1df1602578b
sugijit <b.suldsaikhan@gmail.com>
2023-12-29 23:33:23 +0900

    Top bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#181818",
+        primary: "#d29e0e",
+        secondary: "#e5001c",
+      },
+    },
   },
   plugins: [],
 };
