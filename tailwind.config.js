005a397d228c6081b5e1617207d6105be2713285
sugijit <b.suldsaikhan@gmail.com>
2023-12-30 12:46:01 +0900

    navbar for PC

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        main: ["Montserrat"],
+      },
       colors: {
         dark: "#181818",
         primary: "#d29e0e",
